eb5ba3347979b21fe06c7a3acb40bb961876243d
bhansen0091 <bhansen0091@yahoo.com>
2021-02-18 23:37:46 -0600

    assignment refactor

--- great_number_game/game_app/urls.py
+++ great_number_game/game_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.root),
     path("handle_data", views.handle_data),
-    path("too_low", views.too_low),
-    path("too_high", views.too_high),
-    path("success", views.success),
-    path("start_over", views.start_over)
+    # path("too_low", views.too_low),
+    # path("too_high", views.too_high),
+    # path("success", views.success),
+    path("reset", views.reset)
 ]
--- great_number_game/game_app/views.py
+++ great_number_game/game_app/views.py
@@ -4,37 +4,21 @@ import random
 def root(request):
     if 'number_to_guess' not in request.session:
         request.session['number_to_guess'] = random.randint(0,100)
-    return render(request, "index.html")
+    if 'guess_box' not in request.session:
+        request.session['guess_box'] = -1   
+    context = {
+        "number_to_guess_template": request.session['number_to_guess'],
+        "guess_box_template": int(request.session['guess_box'])
+    }
+    return render(request, "index.html", context)
 
 def handle_data(request):
-    # number_to_guess = request.session['number_to_guess']
-    # user_guess = request.session['guess_box']
-
-    if 'guess_box' not in request.session:
-        request.session['guess_box'] = request.POST['guess_box']
     request.session['guess_box'] = request.POST['guess_box']
     print(request.session['number_to_guess'])
     print(request.session['guess_box'])
-    if request.session['number_to_guess'] > int(request.session['guess_box']):
-        print("too low")
-        return redirect('/too_low')
-    elif request.session['number_to_guess'] < int(request.session['guess_box']):
-        print("too high")
-        return redirect('/too_high')
-    else:
-        print("success")
-    return redirect('/success')
+    return redirect('/')
 
-def too_low(request):
-    return render(request, "too_low.html")
-
-def too_high(request):
-    return render(request, "too_high.html")
-
-def success(request):
-    return render(request, "success.html")
-
-def start_over(request):
+def reset(request):
     request.session.clear()
     return redirect('/')
 
